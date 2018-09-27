d71c8beab2ed3d0431dd1dd840ace86ab7d01977
xiayong <xiayong@meituan.com>
2018-09-27 16:14:17 +0800

    添加pHash比较

--- src/com/meituan/duplicateimage/finder/pHashComparer.java
+++ src/com/meituan/duplicateimage/finder/pHashComparer.java
@@ -5,9 +5,19 @@ import com.meituan.duplicateimage.bean.ImageFile;
 /**
  * pHash比较
  */
-public class pHashComparer implements ImageComparer{
+public class pHashComparer implements ImageComparer {
     @Override
     public boolean isSimilar(ImageFile img1, ImageFile img2) {
-        return false;
+        return distanceChk(img1.mHash, img2.mHash, 0);
+    }
+
+    private boolean distanceChk(String hash1, String hash2, int tv) {
+        int counter = 0;
+        for (int k = 0; k < hash1.length(); k++) {
+            if (hash1.charAt(k) != hash2.charAt(k)) {
+                counter++;
+            }
+        }
+        return counter <= tv;
     }
 }
